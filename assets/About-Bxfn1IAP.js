09338e52a5d526824a2ec12be8c2bbad2fd3c826
shaneguo89757 <shane89757@outlookc.com>
2025-01-17 23:16:39 +0800

    Updates

---
+++ assets/About-Bxfn1IAP.js
@@ -0,0 +1,1 @@
+import{_ as t,c as s,a as o,o as a}from"./index-DRcf3-h7.js";const n={},c={class:"about-container"};function r(_,e){return a(),s("div",c,e[0]||(e[0]=[o("h2",null,"關於我們",-1),o("p",null,"這是一個使用 Vue 3 開發的網站。",-1)]))}const d=t(n,[["render",r],["__scopeId","data-v-f8430454"]]);export{d as default};
